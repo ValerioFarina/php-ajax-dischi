92416827a040759325ca972303aa47402996f2c4
Valerio Farina <valerio.farina.1994@gmail.com>
2020-12-17 15:15:33 +0100

    Inizializzato progetto

---
+++ src/app.js
@@ -0,0 +1,5 @@
+const $ = require('jquery');
+
+$(document).ready(function() {
+    $(".container").append("<h1>Hello World</h1>");
+});
---
+++ versione-php/index.php
@@ -0,0 +1,15 @@
+<?php
+    require 'albums.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Albums - Versione PHP</title>
+        <link rel="stylesheet" href="../dist/app.css">
+    </head>
+    <body>
+        <?php var_dump($albums) ?>
+    </body>
+</html>
