e57cea98644a1f01bb5fdf7ca37f03b9dd426590
Valerio Farina <valerio.farina.1994@gmail.com>
2020-12-17 19:08:41 +0100

    Versione AJAX: aggiunti commenti al file app.js

--- src/app.js
+++ src/app.js
@@ -1,21 +1,34 @@
 const $ = require('jquery');
 
 $(document).ready(function() {
+    // we get the template of an album's card
+    // and we assign it to the variable albumHtml
     var albumHtml = $('#album-template').html();
 
+    // we compile albumHtml in order to get the function albumTemplate
+    // when called, this function returns an instance of albumHtml
     var albumTemplate = Handlebars.compile(albumHtml);
 
+    // we get all the albums
     getAlbums('All');
 
     $("select.genres").change(function() {
+        // when we select a genre, first of all we empty the albums' container
         $(".albums").empty();
 
+        // then, we recover the genre selected
         var selectedGenre = $("select.genres").val();
 
+        // finally, we get the albums that match the selected genre
         getAlbums(selectedGenre);
     });
 
     // ****************** functions ******************
+
+    // this function takes as parameter the string genre, and makes an AJAX request
+    // in case of successful request, the response is an array of objects representing albums that match the genre
+    // we passed to the function as argument
+    // then, for each album in the array, we get a corresponding card, and we append it to the albums' container
     function getAlbums(genre) {
         $.ajax({
             url: 'albums.php',
@@ -25,6 +38,8 @@ $(document).ready(function() {
             },
             success: function(albums) {
                 albums.forEach((album) => {
+                    // for each album in the array albums we get
+                    // the poster's url, the title, the author, the genre and the year
                     var placeholders = {
                         posterUrl: album.poster,
                         title: album.title,
@@ -33,8 +48,11 @@ $(document).ready(function() {
                         year: album.year
                     };
 
+                    // using this informations regarding the current album,
+                    // we "build" a corresponding album's card
                     var albumHtml = albumTemplate(placeholders);
 
+                    // then, we append the album's card to the albums' container
                     $(".albums").append(albumHtml);
                 });
             },
