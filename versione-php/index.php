69d974af3f2094b2f3365fb4df7104e0157ae319
Valerio Farina <valerio.farina.1994@gmail.com>
2020-12-17 15:39:40 +0100

    Versione-PHP: inserite delle card in modo statico

--- versione-php/index.php
+++ versione-php/index.php
@@ -15,9 +15,105 @@
         </header>
 
         <main>
-            <div class="container">
-                <div class="card">
+            <div class="container albums">
+                <div class="album">
+                    <div class="poster">
+                        <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
+                    </div>
+                    <div class="info">
+                        <h4 class="title">
+                            New Jersey
+                        </h4>
+                        <h5 class="author">
+                            Bon Jovi
+                        </h5>
+                        <h5 class="genre">
+                            Rock
+                        </h5>
+                        <h5 class="year">
+                            1988
+                        </h5>
+                    </div>
+                </div>
 
+                <div class="album">
+                    <div class="poster">
+                        <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
+                    </div>
+                    <div class="info">
+                        <h4 class="title">
+                            New Jersey
+                        </h4>
+                        <h5 class="author">
+                            Bon Jovi
+                        </h5>
+                        <h5 class="genre">
+                            Rock
+                        </h5>
+                        <h5 class="year">
+                            1988
+                        </h5>
+                    </div>
+                </div>
+
+                <div class="album">
+                    <div class="poster">
+                        <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
+                    </div>
+                    <div class="info">
+                        <h4 class="title">
+                            New Jersey
+                        </h4>
+                        <h5 class="author">
+                            Bon Jovi
+                        </h5>
+                        <h5 class="genre">
+                            Rock
+                        </h5>
+                        <h5 class="year">
+                            1988
+                        </h5>
+                    </div>
+                </div>
+
+                <div class="album">
+                    <div class="poster">
+                        <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
+                    </div>
+                    <div class="info">
+                        <h4 class="title">
+                            New Jersey
+                        </h4>
+                        <h5 class="author">
+                            Bon Jovi
+                        </h5>
+                        <h5 class="genre">
+                            Rock
+                        </h5>
+                        <h5 class="year">
+                            1988
+                        </h5>
+                    </div>
+                </div>
+
+                <div class="album">
+                    <div class="poster">
+                        <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
+                    </div>
+                    <div class="info">
+                        <h4 class="title">
+                            New Jersey
+                        </h4>
+                        <h5 class="author">
+                            Bon Jovi
+                        </h5>
+                        <h5 class="genre">
+                            Rock
+                        </h5>
+                        <h5 class="year">
+                            1988
+                        </h5>
+                    </div>
                 </div>
             </div>
         </main>
